ceb6cac4fe29565b16f337f665064261bd96a300
Reshzy <reshzy581@gmail.com>
2026-04-03 21:21:49 +0800

    Update font configurations and enhance CSS styling
    - Changed default sans font to 'Hind' and added 'Montserrat' for display in Tailwind CSS configuration.
    - Updated CSS variables for body and heading fonts in app.css.
    - Modified HTML to load the new font styles from Bunny Fonts.
    - Added styles for heading elements to use the new heading font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Hind', ...defaultTheme.fontFamily.sans],
+                display: ['Montserrat', ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 app: {
